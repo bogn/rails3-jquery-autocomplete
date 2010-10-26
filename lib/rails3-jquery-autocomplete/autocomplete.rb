f9a0f403f9694d8902e3bb422f6eea491e9685c7
Bogomir Engel <bogomir.engel@gmx.de>
2010-10-26 18:36:06 +0200

    - Adding the possibility to use the full text search engine Sphinx via the Thinking Sphinx gem. Thinking Sphinx is used for all models that define Sphinx indexes. - New mode allowing to search across multiple models or multiple fields in one model. Works only for Thinking Sphinx currently. See the following description to see how to define what to search for:
    single model, single field (nothing new, but uses Thinking Sphinx if the model has an index on it)
    autocomplete :article, :text
    
    single model, multiple fields
    autocomplete :article, [:title, :text]
    
    multiple models, all (indexed) fields
    autocomplete :search,  [Article, Comment, User]

--- lib/rails3-jquery-autocomplete/autocomplete.rb
+++ lib/rails3-jquery-autocomplete/autocomplete.rb
@@ -21,20 +21,32 @@ module Rails3JQueryAutocomplete
   #
   #
   module ClassMethods
-    def autocomplete(object, method, options = {})
+    def autocomplete(pool_name, pool_items, options = {})
+      single_method = !pool_items.is_a?(Array)
+      multiple_models = (!single_method and pool_items.none?{|i| i.is_a? Symbol })
 
-      define_method("autocomplete_#{object}_#{method}") do
+      if !single_method and options[:display_value].blank? 
+        raise Exception.new("You have to provide a function name with the display_value " \
+        "option if you want to search across multiple fields/models")
+      end
+      
+      method_name = single_method ? "#{pool_name}_#{pool_items}" : "#{pool_name}"
+
+      define_method("autocomplete_#{method_name}") do
 
         term = params[:term]
 
         if term && !term.empty?
-          items = get_items(:model => get_object(object), \
-            :options => options, :term => term, :method => method) 
+          if multiple_models
+            items = get_items_from_multiple_sources(pool_items, term, options)
+          else
+            items = get_items_from_single_model(get_object(pool_name), pool_items, term, options)
+          end
         else
           items = {}
         end
 
-        render :json => json_for_autocomplete(items, options[:display_value] ||= method)
+        render :json => json_for_autocomplete(items, options[:display_value] ||= pool_items)
       end
     end
   end
--- lib/rails3-jquery-autocomplete/helpers.rb
+++ lib/rails3-jquery-autocomplete/helpers.rb
@@ -18,8 +18,12 @@ module Rails3JQueryAutocomplete
 
     # Returns a symbol representing what implementation should be used to query
     # the database and raises *NotImplementedError* if ORM implementor can not be found
-    def get_implementation(object) 
-      if object.superclass.to_s == 'ActiveRecord::Base'
+    def get_implementation(pool)
+      object = pool.is_a?(Array) ? pool[0] : pool 
+
+      if object.included_modules.collect(&:to_s).include?('ThinkingSphinx::ActiveRecord')
+        :thinking_sphinx
+      elsif object.superclass.to_s == 'ActiveRecord::Base'
         :activerecord
       elsif object.included_modules.collect(&:to_s).include?('Mongoid::Document')
         :mongoid
@@ -29,7 +33,7 @@ module Rails3JQueryAutocomplete
     end
 
     # Returns the order parameter to be used in the query created by get_items
-    def get_order(implementation, method, options)
+    def get_order(implementation, fields, options)
       order = options[:order]
 
       case implementation
@@ -40,10 +44,14 @@ module Rails3JQueryAutocomplete
               [sfields[0].downcase.to_sym, sfields[1].downcase.to_sym]
             end
           else
-            [[method.to_sym, :asc]]
+            if fields.is_a? Array
+              fields.collect{|f| [f.to_sym, :asc] }
+            else
+              [[fields.to_sym, :asc]]
+            end
           end
-        when :activerecord then 
-          order || "#{method} ASC"
+        when :activerecord, :thinking_sphinx then
+          order ? order : fields.collect{|f| "#{f.to_sym} ASC" }.join(", ")
       end
     end
 
@@ -52,28 +60,54 @@ module Rails3JQueryAutocomplete
       options[:limit] ||= 10
     end
   
-    # Queries selected database
+    # Gets items from the single model with the adequate db interface
     #
-    #   items = get_items(:model => get_object(object), :options => options, :term => term, :method => method) 
-    def get_items(parameters)
-
-      model = parameters[:model]
-      method = parameters[:method]
-      options = parameters[:options]
-      term = parameters[:term]
+    #   items = get_items_from_single_model(:model => get_object(object), :fields => fields, :term => term, :options => options) 
+    def get_items_from_single_model(model, fields, term, options)
       is_full_search = options[:full]
 
       limit = get_limit(options)
       implementation = get_implementation(model)
-      order = get_order(implementation, method, options)
+      order = get_order(implementation, fields, options)
 
       case implementation
         when :mongoid
           search = (is_full_search ? '.*' : '^') + term + '.*'
-          items = model.where(method.to_sym => /#{search}/i).limit(limit).order_by(order)
+          items = model.where(fields.to_sym => /#{search}/i).limit(limit).order_by(order)
         when :activerecord
-          items = model.where(["LOWER(#{method}) LIKE ?", "#{(is_full_search ? '%' : '')}#{term.downcase}%"]) \
+          items = model.where(["LOWER(#{fields}) LIKE ?", "#{(is_full_search ? '%' : '')}#{term.downcase}%"]) \
             .limit(limit).order(order)
+        when :thinking_sphinx
+          query = "#{(is_full_search ? '*' : '')}#{term}*"
+          if fields.is_a? Array
+            items = model.search(fields.collect{|f| "@#{f.to_sym} #{query}" }.join(" | "), \
+                      :match_mode => :boolean, :per_page => limit, :order => order)
+          else
+            items = model.search(:conditions => {fields => query}, :per_page => limit, :order => order)
+          end
+      end
+    end
+
+    # Gets items from multiple models with the adequate db interface
+    #
+    #   items = get_items(:pool => search_pool, :options => options, :term => term, :method => method) 
+    def get_items_from_multiple_sources(pool_items, term, options)
+      is_full_search = options[:full]
+      
+      limit = get_limit(options)
+      implementation = get_implementation(pool_items) 
+      order = get_order(implementation, "created_at", options)
+
+      case implementation
+        when :mongoid
+          search = (is_full_search ? '.*' : '^') + term + '.*'
+          items = pool.where(method.to_sym => /#{search}/i).limit(limit).order_by(order)
+        when :activerecord
+          items = pool.where(["LOWER(#{method}) LIKE ?", "#{(is_full_search ? '%' : '')}#{term.downcase}%"]) \
+            .limit(limit).order(order)
+        when :thinking_sphinx
+          items = ThinkingSphinx.search("#{(is_full_search ? '*' : '')}#{term}*", :classes => pool_items, \
+                    :per_page => limit, :order => order)
       end
     end
 
